8fb478c3480abb36c4e7ade21eb3872d2c5bcc5c
santoshbiradarsantu <167407248+santoshbiradarsantu@users.noreply.github.com>
2024-05-02 01:07:10 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 50;
+        int c = a * b;
+        System.out.println("total value is :" + c);
+    }
+
+}
